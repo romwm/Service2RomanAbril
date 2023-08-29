44675662dbb2a21a8f81c30410c2fe5cd1845cf3
romwm <73566034+romwm@users.noreply.github.com>
2023-08-29 10:31:27 -0300

    Add files via upload

---
+++ Service-RomanAbril-main/Service Román Abril/WindowsService1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IncrementService service = new IncrementService();
+
+            ServiceBase.Run(service);
+        }
+    }
+    
+}
